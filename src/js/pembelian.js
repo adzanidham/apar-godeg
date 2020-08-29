bccd0d1fe9100a2383cb47a6b6025275a8e3b9e7
adzanidham <adzani234@gmail.com>
2020-08-29 08:28:04 +0700

    create objek pembelian

---
+++ src/js/pembelian.js
@@ -0,0 +1,13 @@
+const pembelian = {
+  pembelianId: '',
+  sellAs: '',
+  date: '',
+  faktur: '',
+  nmCustomer: '',
+  alamat: '',
+  kota: '',
+  phone: '',
+  items: [],
+}
+
+export default pembelian
